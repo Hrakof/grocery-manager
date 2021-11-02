db57a47c420e979a14f6ea793a75e3a2e29e0994
Hrakof <karmacsi.peter@freemail.hu>
2021-11-02 20:41:45 +0100

    fridge tab

--- lib/screens/households/household_details/tabs/fridge_tab.dart
+++ lib/screens/households/household_details/tabs/fridge_tab.dart
@@ -1,12 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_manager/blogic/provider/households/household_details_state.dart';
+import 'package:grocery_manager/models/household/household.dart';
+import 'package:grocery_manager/repositories/item/item_repository.dart';
+import 'package:grocery_manager/screens/items/item_creation/item_creation_screen.dart';
+import 'package:grocery_manager/screens/items/item_details/item_details_screen.dart';
+import 'package:grocery_manager/widgets/item_list.dart';
+import 'package:provider/provider.dart';
 
 class FridgeTab extends StatelessWidget {
   const FridgeTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Fridge tab'),
+    final state = context.watch<HouseholdDetailsState>();
+    return Stack(
+      children: [
+        state.fridgeItems == null ?
+        const Center(
+          child: CircularProgressIndicator(),
+        )
+            :
+        ItemList(
+          state.fridgeItems!,
+          checkedItemIds: state.selectedFridgeItemIds,
+          onItemChecked: (item){
+            state.itemChecked(item.id, ItemCollection.fridge);
+          },
+          onItemTapped: (item){
+            Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+              return ItemDetailsScreen(
+                householdId: state.household!.id,
+                itemCollection: ItemCollection.fridge,
+                itemId: item.id,
+              );
+            }));
+          },
+        ),
+        Selector<HouseholdDetailsState, Household?>(
+          selector: (_, state) => state.household,
+          builder: (_, household, __){
+            if(household != null) {
+              return Positioned(
+                bottom: 40,
+                right: 30,
+                child: FloatingActionButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => ItemCreationScreen(
+                      householdId: household.id,
+                      itemCollection: ItemCollection.fridge,
+                    )));
+                  },
+                  child: const Icon(Icons.add),
+                ),
+              );
+            }
+            return const SizedBox();
+          },
+        ),
+      ],
     );
   }
 }
